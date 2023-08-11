c5e091ce85d4b1aa54560746a068a6ad42263140
kiminn <808010x@gmail.com>
2023-08-11 23:22:31 +0900

    다크모드 useContext사용하여 상태관리, 구현

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import './App.css';
+import Page from './components/Page';
+import { ThemeContext } from './context/ThemeContext';
+import { UserContext } from './context/UserContext';
+
+function App() {
+    const [isDark, setIsDark] = useState(false); // 기본적으로 isDark가 꺼져있음
+
+    return (
+        <UserContext.Provider value={'kimi'}>
+            <ThemeContext.Provider value={{ isDark, setIsDark }}>
+                {/* value 안에는 전달하고자하는 데이터를 집어넣는다
+             모든 하위컴포넌트에 전달해야하는 isDark, setIsDark 넣어주기
+          props를 사용하지않고 접근할 수 있다 */}
+                {/* <Page isDark={isDark} setIsDark={setIsDark}/> */}
+                <Page />
+            </ThemeContext.Provider>
+        </UserContext.Provider>
+    );
+}
+
+export default App;
